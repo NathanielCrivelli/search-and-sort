d0260a0b431ed14f9716cc2b36cc215103230966
Nathaniel Crivelli <nathaniel.crivelli@mrjaffesclass.com>
2022-09-23 12:46:33 -0700

    09/23/22

--- search/Sequential.java
+++ search/Sequential.java
@@ -1,4 +1,7 @@
 package search;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 
 /**
@@ -17,25 +20,29 @@ public class Sequential
     // Your algorithm goes here!
     int psn = 0;
     Scanner input = new Scanner(System.in);
-    if (psn > -1)
+    int goal = System.in;
+    while (psn > -1)
     {
-
+      List[] result = new List[psn];
       psn += 1;
-    } else {
-
-      psn = -1;
+      System.out.println("SUCCESS! NUMBER IS AT POSITION " + psn +"!");
+      if (System.in == goal)
+      {
+          
+      }
     }
+    return psn;
   }
   
   public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+    int[] list = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
       31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
       73,54,51,25,33,20,52,79,97,70,54,63,49};
     int left, right, mid;
     
-    printResult(82, search(arr, 82) == 25);
-    printResult(49, search(arr, 49) == arr.length-1);
-    printResult(-4, search(arr, -4) == -1);
+    printResult(82, search(list, 82) == 25);
+    printResult(49, search(list, 49) == list.length-1);
+    printResult(-4, search(list, -4) == -1);
   }
   
   public static void printResult(int target, boolean result) {
